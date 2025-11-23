04d8d9427a5b06ce81445e014a5768e33a202361
Fernando Nina <Fernandonina611@gmail.com>
2025-11-23 14:12:55 -0400

    feat: :pacjage: instalacion de shadcn para componentes ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <div>Hola mundo</div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   );
 }
