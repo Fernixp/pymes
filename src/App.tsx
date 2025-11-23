6f0cd4a03f20e64f44209128a32d7d981c14beb1
Fernando Nina <Fernandonina611@gmail.com>
2025-11-23 16:55:06 -0400

    feat: :sparkles: agreegando navbar con shadcn.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { HeroSection } from "./components/home/hero-section";
 import { FeaturesSection } from "./components/home/features-section";
 import { HowItWorksSection } from "./components/home/hows-it-works-section";
 import { TheorySection } from "./components/home/theory-section";
 import { ScrollToTop } from "./components/home/scroll-to-top";
+import { Navbar01 } from "./components/ui/shadc-io/navbar-01";
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="min-h-svh bg-background text-foreground transition-colors duration-300">
-        <header className="border-b bg-card/50 backdrop-blur-sm sticky top-0 z-10">
+        <header className="border-b bg-card/50 sticky top-0 z-10">
           <div className="container mx-auto px-4">
-            <Navbar />
+          <Navbar01 />
           </div>
         </header>
         <main className="container mx-auto px-4 flex flex-col items-center justify-center py-10 text-center space-y-3">
