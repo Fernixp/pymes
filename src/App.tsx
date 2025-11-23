219689b6987325d4ad0c690e0c556a7865b27d6f
Fernando Nina <Fernandonina611@gmail.com>
2025-11-23 14:17:27 -0400

    chore: instalando fuente Outfit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maiores enim magni nemo sed quod, dolores libero ad vel et officia hic repellendus quia incidunt impedit quo, voluptate nihil culpa.
       <Button>Click me</Button>
     </div>
     </>
